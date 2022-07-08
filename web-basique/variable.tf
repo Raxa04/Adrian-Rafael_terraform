733910da2d341d558d727b6674c778b28afb2ea7
Raxa <raxa04@gmail.com>
2022-07-08 15:51:30 +0200

    blblblblbl

---
+++ web-basique/variable.tf
@@ -0,0 +1,14 @@
+variable "project_id" {
+  type        = string
+  default     = "adrian-rafael-terraform"
+}
+
+variable "region" {
+  type        = string
+  default     = "europe-north1"
+}
+
+variable "zone" {
+  type        = string
+  default     = "europe-north1-b"
+}
