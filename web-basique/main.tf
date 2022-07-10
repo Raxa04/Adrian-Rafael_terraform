301ecd48879fd8f69f760e8059e867e501b10155
Raxa <raxa04@gmail.com>
2022-07-10 17:06:48 +0200

    truc

--- web-basique/main.tf
+++ web-basique/main.tf
@@ -23,6 +23,16 @@ resource "google_compute_instance" "vm_instance" {
   }
 }
 
+resource "google_sql_database_instance" "main" {
+  name             = "main-instance"
+  database_version = "POSTGRES_14"
+
+  settings {
+    # Second-generation instance tiers are based on the machine
+    # type. See argument reference below.
+    tier = "db-f1-micro"
+  }
+}
 
 resource "google_compute_network" "vpc_network" {
   name                    = "terraform-network"
